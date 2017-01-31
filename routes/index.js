3294712dc5fabd06534605927313c8de6acfac62
kiran0010 <totan0010@gmail.com>
2017-01-31 13:40:37 +0530

    ksjs

--- routes/index.js
+++ routes/index.js
@@ -1,77 +1,4 @@
-// var express = require('express');
-// var router = express.Router();
-// var nodemailer = require('nodemailer');
-// var hbs = require('nodemailer-express-handlebars');
 
-
-// // // var transporter = nodemailer.createTransport({
-// // //     host: 'smtp-pulse.com'
-// // //     port: 465,
-// // //     secure: true,
-// // //     auth: {
-// // //     	ID: '181813834307ae2d310cf638197e51c4'
-// // //     	Secret: 'd607252db7cbb674f4c7c9879d5982f6'
-// // //     }
-// // // });
-
-// // // var transporter = nodemailer.createTransport({
-// // //     transport: 'ses', // loads nodemailer-ses-transport
-// // //     accessKeyId: 'AWSACCESSKEY',
-// // //     secretAccessKey: 'AWS/Secret/key'
-// // // });
-
-// // var smtpConfig = {
-// //     host: 'smtp-pulse.com',
-// //     port: 465,
-// //     secure: true, // use SSL
-// //     auth: {
-// //         user: 'user@gmail.com',
-// //         pass: 'pass'
-// //     }
-// // };
-
-// // var poolConfig = {
-// //     pool: true,
-// //     host: 'smtp-pulse.com',
-// //     port: 465,
-// //     secure: true, // use SSL
-// //     auth: {
-// //         user: 'user@gmail.com',
-// //         pass: 'pass'
-// //     }
-// // };
-
-// // var directConfig = {
-// //     name: 'hostname' // must be the same that can be reverse resolved by DNS for your IP
-// // };
-
-
-// // mailer use('compile', hsb({
-// // 	viewPath: 'views/email',
-// // 	extName: '.hbs'
-// // }))
-// // /* GET home page. */
-// // router.get('/', function(req, res, next) {
-// //   res.render('index', { title: 'Express' });
-// // });
-
-// // router.get('/sendmail', function(req, res, next) {
-// //   mailer.sendMail({
-// //   	from: 'totan0010@tmail.com',
-// //   	to: 'totan0010@gmail.com',
-// //   	subject: 'Password',
-// //   	template: 'recover',
-// //   	context: {}
-// //   },function(err, response){
-// //   	if(err){
-// //   		res send('bad mail');
-// //   	}
-// //   	res send('good mail');
-// //   });
-// // });
-
-
-// module.exports = router;
 
 
 
@@ -92,15 +19,7 @@ router.post('/mailsender', function (req, res) {
      var sub = req.body.subject;
      console.log(sub);
      var comment = req.body.comment;
-     // console.log(comment);
-    // var transporter = nodemailer.createTransport({
-    //     service: 'Gmail',
-    //     auth: {
-    //         user: 'gorai.ranjit13@gmail.com', // Your email id
-    //         pass: '8119061245' // Your password
-    //     }
-    // });
-
+   
 
 
     var transporter = nodemailer.createTransport({
